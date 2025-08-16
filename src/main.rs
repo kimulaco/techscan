2c17701a68cfa545955048a110abe4889cf1d0de
kimulaco <kimulaco@gmail.com>
2025-08-17 00:58:46 +0900

    feat: implemented parse CLI args and scan files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::Parser;
+use walkdir::WalkDir;
+
+#[derive(Parser)]
+#[command(name = "tech-scan")]
+#[command(about = "A tool for analyzing and visualizing technology stacks in codes.")]
+struct Cli {
+    dir: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("Processing directory: {}", cli.dir);
+
+    let entries = WalkDir::new(&cli.dir);
+    for entry in entries {
+        match entry {
+            Ok(entry) => {
+                let file_type = entry.file_type();
+                let path = entry.path();
+
+                if file_type.is_file() {
+                    println!("{}", &path.to_string_lossy());
+                }
+            }
+            Err(e) => {
+                eprintln!("Error reading entry: {}", e);
+            }
+        }
+    }
 }
