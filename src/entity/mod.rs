8980333ce4f0644f128509c9eac0212d43d2d0b0
kimulaco <11986753+kimulaco@users.noreply.github.com>
2025-08-31 17:26:50 +0900

    feat: provide lib (#8)
    * feat: add `lib.rs`
    * fix: module dependencies
    * test: add `ConfigBuilder` tests

--- src/entity/mod.rs
+++ src/entity/mod.rs
@@ -1,8 +1,10 @@
+pub mod app_config;
 pub mod file;
 pub mod language;
 pub mod language_report;
 pub mod language_scanner_options;
 
+pub use app_config::AppConfig;
 pub use file::File;
 pub use language::Language;
 pub use language_report::{LanguageReport, LanguageReportItem};
